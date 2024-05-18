04045df1df72da1e0b6bccc4e2081bb598a5e553
silqwer <silqwer@gmail.com>
2024-05-18 13:01:57 +0900

    feat: 홈탭 만들면서 Context API 적용해보기

--- src/app/(afterLogin)/home/layout.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "@/app/page.module.css";
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function HomeLayout({ children }: Props) {
-  return <div className={styles.container}>{children}</div>;
-}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import style from "./home.module.css";
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "../_component/Post";
 
-export default function page() {
-  return <div>page</div>;
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
